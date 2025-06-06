b2b12f4f692b2d25787d90701c5c44307e64799a
Sneha Sudarshan <sneha.sudarshan28@gmail.com>
2025-06-06 15:38:41 +0530

    Updated find-item.cpp logic

--- src/Ch01-Arrays/01_07/begin/find-item.cpp
+++ src/Ch01-Arrays/01_07/begin/find-item.cpp
@@ -6,9 +6,32 @@ template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
     // TODO: implement
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]==target)
+        {
+             return i;
+             
+        }
+    }
+    return -1;
 }
 
 int main()
 {
+    int numbers[] {4, 7, 12, 3, 9, 17, 29};
+    
+    size_t size = sizeof(numbers) / sizeof(numbers[0]);
+     int target;
+     cin>>target;
+     int index=findItem(numbers,size,target);
+     if(index==-1)
+     {
+        cout<<"target not found in the array"<<endl;
+     }
+     else{
+        cout<<"target found at index "<<index<<endl;
+     }
+    
     return 0;
 }
