4252a5284e9f8647ee8b0e4cf1e039b0d77630b9
Sneha Sudarshan <sneha.sudarshan28@gmail.com>
2025-06-06 14:52:59 +0530

    Updated find-primes.cpp logic

--- src/Ch01-Arrays/01_05/begin/find-primes.cpp
+++ src/Ch01-Arrays/01_05/begin/find-primes.cpp
@@ -2,10 +2,28 @@
 #include <vector>
 
 using namespace std;
-
+bool isPrimes(int num)
+{
+    if(num<2)return false;
+    for(int i=2;i<=sqrt(num);i++)
+    {
+        if(num%i!=0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 void findPrimes(const int arr[], size_t size, std::vector<int> &primes)
 {
     // TODO: Implement
+    for(int i=0;i<size;i++)
+    {
+        if(isPrimes(arr[i]))
+        {
+            primes.push_back(arr[i]);
+        }
+    }
 }
 
 int main()
