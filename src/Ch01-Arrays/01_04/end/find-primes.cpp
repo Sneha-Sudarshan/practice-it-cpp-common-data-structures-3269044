aa28ba21a19abf73d3ceba41293ccdf893c90993
Sneha Sudarshan <sneha.sudarshan28@gmail.com>
2025-06-06 14:52:02 +0530

    Updated find-primes.cpp logic

--- src/Ch01-Arrays/01_04/end/find-primes.cpp
+++ src/Ch01-Arrays/01_04/end/find-primes.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool isPrime(int num)
 {
     if(num<2) return false;
-    for(int i=2;i<sqrt(num);i++)
+    for(int i=2;i<=sqrt(num);i++)
     {
         if(num%i!=0)
         {
